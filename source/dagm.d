70a0f3754695157ab5b4e1afe322a504e829e842
simon <simon.lederhilger@gmail.com>
2026-09-13 08:15:33 +0200

    Color palette output

--- source/dagm.d
+++ source/dagm.d
@@ -1,21 +1,43 @@
 module dagm;
 
-import std.stdio : writefln;
+import std.stdio : writef;
 import std.math : sqrt;
 import agm : AGM;
 import ellipticity : Ellipticity;
 import jacobi : cd;
+import palette : palette;
+import vectrix : Vectrix;
+
+double[] linspace(double start, double end, size_t N) @safe pure
+{
+	auto grid = new double[N];
+	if (N == 1)
+	{
+		grid[0] = start;
+		return grid;
+	}
+	immutable double space = (end - start) / (N - 1);
+	foreach (i; 0 .. N)
+	{
+		grid[i] = start + i * end;
+	}
+	return grid;
+}
 
 void main()
 {
+	const(double)[64] x = linspace(0, 1, 64);
+	auto α = Vectrix(.5, .5, .5);
+	auto β = Vectrix(.5, .5, .5);
+	auto γ = Vectrix(1, 1, 1);
+	auto δ = Vectrix(0, .1, .2);
 	auto ellipticity = Ellipticity(sqrt(.5));
-	writefln("K = %.15f", ellipticity.K);
-	writefln("cd(2K) = %.15f", cd(ellipticity, 2 * ellipticity.K));
-
-	auto agm = AGM(1.0, sqrt(.5), sqrt(.5), 1e-16);
-	foreach (i; 0 .. agm.length)
+	auto colors = palette(x, α, β, γ, δ, ellipticity);
+	foreach (color; colors)
 	{
-		writefln("i=%d  A=%.15f  C=%.15f", i, agm.A[i], agm.C[i]);
+		immutable ubyte r = cast(ubyte)(color.r * 255 + .5);
+		immutable ubyte g = cast(ubyte)(color.g * 255 + .5);
+		immutable ubyte b = cast(ubyte)(color.b * 255 + .5);
+		writef(`<span style="color:rgb(%d,%d,%d)">█</span>`, r, g, b);
 	}
-	writefln("length = %d", agm.length);
 }
--- source/palette.d
+++ source/palette.d
@@ -2,3 +2,48 @@ module palette;
 
 import ellipticity : Ellipticity;
 import jacobi : cd;
+import vectrix : Vectrix;
+
+Vectrix palette(double x, Vectrix α, Vectrix β, Vectrix γ, Vectrix δ, ref const Ellipticity ellipticity) @safe @nogc pure nothrow
+{
+	pragma(inline, true);
+
+	immutable double period = 4.0 * ellipticity.K;
+	immutable double ξ = period * (γ.r * x + δ.r);
+	immutable double η = period * (γ.g * x + δ.g);
+	immutable double ζ = period * (γ.b * x + δ.b);
+
+	return Vectrix(
+	       α.r + β.r * cd(ellipticity, ξ),
+	       α.g + β.g * cd(ellipticity, η),
+	       α.b + β.b * cd(ellipticity, ζ)
+	);
+}
+
+void palette(const(double)[] x, Vectrix α, Vectrix β, Vectrix γ, Vectrix δ, ref const Ellipticity ellipticity, Vectrix[] spectrum) @safe @nogc pure nothrow
+in (x.length == spectrum.length)
+{
+	immutable double period = 4.0 * ellipticity.K;
+	immutable double γR = γ.r * period, γG = γ.g * period, γB = γ.b * period;
+	immutable double δR = δ.r * period, δG = δ.g * period, δB = δ.b * period;
+
+	foreach (i, X; x)
+	{
+		immutable double ξ = γR * X + δR;
+		immutable double η = γG * X + δG;
+		immutable double ζ = γB * X + δB;
+
+		spectrum[i] = Vectrix(
+			    α.r + β.r * cd(ellipticity, ξ),
+			    α.g + β.g * cd(ellipticity, η),
+			    α.b + β.b * cd(ellipticity, ζ)
+		);
+	}
+}
+
+Vectrix[] palette(const(double)[] x, Vectrix α, Vectrix β, Vectrix γ, Vectrix δ, ref const Ellipticity ellipticity) @safe
+{
+	auto spectrum = new Vectrix[x.length];
+	palette(x, α, β, γ, δ, ellipticity);
+	return spectrum;
+}
