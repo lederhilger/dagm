a8d4c4b2ec2faf16baf343dc546f749a6672f715
simon <simon.lederhilger@gmail.com>
2026-09-14 15:42:17 +0200

    Added clamp to avoid overflow; append line

--- source/dagm.d
+++ source/dagm.d
@@ -1,6 +1,6 @@
 module dagm;
 
-import std.stdio : writef;
+import std.stdio : writef, writeln;
 import std.math : sqrt, PI;
 import std.conv : to;
 import agm : AGM;
@@ -8,6 +8,7 @@ import ellipticity : Ellipticity;
 import jacobi : cd;
 import palette : palette;
 import vectrix : Vectrix, add;
+import std.algorithm.comparison : clamp;
 
 double[] linspace(double start, double end, size_t N) @safe pure
 {
@@ -29,10 +30,10 @@ void main(string[] args)
 {
 	immutable double k = args.length > 1 ? to!double(args[1]) : sqrt(.5);
 	const(double)[] x = linspace(0, 1, 64);
-	auto α = Vectrix(.5, .5, .5);
-	auto β = Vectrix(.5, .5, .5);
-	auto γ = Vectrix(1, 1, 1);
-	auto δ = Vectrix(0, .1, .2);
+	auto α = Vectrix(1.0, 0.213, 0.325);
+	auto β = Vectrix(1.0, 0.379, 0.31);
+	auto γ = Vectrix(0.09, 0.305, 0.574);
+	auto δ = Vectrix(0.618, 0.67, 0.516);
 	Vectrix[] colors;
 
 	if (k == 1.0)
@@ -47,9 +48,10 @@ void main(string[] args)
 	}
 	foreach (color; colors)
 	{
-		immutable ubyte r = cast(ubyte)(color.r * 255 + .5);
-		immutable ubyte g = cast(ubyte)(color.g * 255 + .5);
-		immutable ubyte b = cast(ubyte)(color.b * 255 + .5);
+		immutable ubyte r = cast(ubyte)(clamp(color.r, 0.0, 1.0) * 255 + .5);
+		immutable ubyte g = cast(ubyte)(clamp(color.g, 0.0, 1.0) * 255 + .5);
+		immutable ubyte b = cast(ubyte)(clamp(color.b, 0.0, 1.0) * 255 + .5);
 		writef(`<span style="color:rgb(%d,%d,%d)">█</span>`, r, g, b);
 	}
+	writeln();
 }
